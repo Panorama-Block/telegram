aa8be7574e1a576eaddad7cc77a33f82646aad71
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2026-03-05 21:46:48 -0300

    fix logos

--- apps/miniapp/src/modules/landing/partners/index.tsx
+++ apps/miniapp/src/modules/landing/partners/index.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import icp from '../../../../public/landing/partners/icp.svg'
 import xrp from '../../../../public/landing/partners/xrp.svg'
 import chainlink from '../../../../public/landing/partners/chainlink.svg'
-import avax from '../../../../public/landing/partners/avax.svg'
+import avax from '../../../../public/landing/partners/avalanche.svg'
 import stacks from '../../../../public/landing/partners/stacks.svg'
 import morpheus from '../../../../public/landing/partners/morpheus.svg'
 import inteli from '../../../../public/landing/partners/inteli.svg'
 import lmu from '../../../../public/landing/partners/lmu.svg'
 import uniswap from '../../../../public/landing/partners/uniswap.svg'
+import mbtc from '../../../../public/landing/partners/m_btc.svg'
+import ucla from '../../../../public/landing/partners/ucla.svg'
 
 const images = [
   { src: icp, alt: 'Internet Computer' },
@@ -19,36 +21,36 @@ const images = [
   { src: inteli, alt: 'Inteli' },
   { src: lmu, alt: 'LMU' },
   { src: uniswap, alt: 'Uniswap' },
+  { src: mbtc, alt: 'mBTC' },
+  { src: ucla, alt: 'UCLA' },
 ]
+
+const PartnerCard = ({ image }: { image: { src: any; alt: string; extraClass?: string } }) => (
+  <div className="flex items-center justify-center w-[270px] h-36 transition-transform duration-300 ease-out hover:scale-110 hover:z-10 cursor-pointer">
+    <Image src={image.src} alt={image.alt} className={`max-h-[120px] sm:max-h-[192px] lg:max-h-36 w-auto object-contain ${image.extraClass || ''}`} width={270} height={144} />
+  </div>
+)
 
 const Partners = () => {
   return (
     <div className="mt-16 md:mt-32 lg:mt-40">
       <h2 className="text-landing-title text-center text-2xl lg:text-3xl lg:text-4xl px-14 mb-4 md:mb-8">Proudly Colaborating with:</h2>
-      <div className="relative flex overflow-x-hidden">
-        <div className="animate-marquee whitespace-nowrap flex my-8 gap-16 pr-16">
+      <div className="relative flex overflow-x-hidden group">
+        <div className="animate-marquee group-hover:[animation-play-state:paused] whitespace-nowrap flex my-8 gap-16 pr-16">
           {images.map((image, index) => (
-            <div key={index} className="flex items-center justify-center w-[180px] h-24">
-              <Image src={image.src} alt={image.alt} className="max-h-20 sm:max-h-32 lg:max-h-24 w-auto object-contain" width={180} height={96} />
-            </div>
+            <PartnerCard key={index} image={image} />
           ))}
           {images.map((image, index) => (
-            <div key={`second-${index}`} className="flex items-center justify-center w-[180px] h-24">
-              <Image src={image.src} alt={image.alt} className="max-h-20 sm:max-h-32 lg:max-h-24 w-auto object-contain" width={180} height={96} />
-            </div>
+            <PartnerCard key={`second-${index}`} image={image} />
           ))}
         </div>
 
-        <div className="absolute top-0 animate-marquee2 whitespace-nowrap flex my-8 gap-16 pr-16">
+        <div className="absolute top-0 animate-marquee2 group-hover:[animation-play-state:paused] whitespace-nowrap flex my-8 gap-16 pr-16">
           {images.map((image, index) => (
-            <div key={`second-${index}`} className="flex items-center justify-center w-[180px] h-24">
-              <Image src={image.src} alt={image.alt} className="max-h-20 sm:max-h-32 lg:max-h-24 w-auto object-contain" width={180} height={96} />
-            </div>
+            <PartnerCard key={`second-${index}`} image={image} />
           ))}
           {images.map((image, index) => (
-            <div key={`third-${index}`} className="flex items-center justify-center w-[180px] h-24">
-              <Image src={image.src} alt={image.alt} className="max-h-20 sm:max-h-32 lg:max-h-24 w-auto object-contain" width={180} height={96} />
-            </div>
+            <PartnerCard key={`third-${index}`} image={image} />
           ))}
         </div>
       </div>
